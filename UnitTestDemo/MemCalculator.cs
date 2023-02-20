48eaca52e8cff3d414f85e97f7caacfb6ff465da
ecpay CY <>
2023-02-20 15:42:42 +0800

    Add MemCalculator.cs 計算機類別 做測試用

---
+++ UnitTestDemo/MemCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTestDemo;
+
+public class MemCalculator
+{
+    // 簡易計算機功能
+    private int sum = 0;
+
+    public void Add(int number)
+    {
+        sum += number;
+    }
+
+    public int Sum()
+    {
+        int temp = sum;
+        sum = 0;
+        return temp;
+    }
+}
