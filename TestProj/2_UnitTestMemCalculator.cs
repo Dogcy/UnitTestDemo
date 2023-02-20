1a0eab063158775bc58d4ba43fd7ea9cc48f2f2b
ecpay CY <>
2023-02-20 18:01:56 +0800

    UnitTest2 新增

--- TestProj/2_UnitTestMemCalculator.cs
+++ TestProj/2_UnitTestMemCalculator.cs
@@ -11,10 +11,29 @@ public class UnitTestMemCalculator
     {
         _memCalculator = new MemCalculator();
     }
+
+    // 簡易的測試
+
     [Test]
     public void Sum_ByDefault_Returns0()
     {
         int lastSum = _memCalculator.Sum();
-        Assert.AreEqual(0, lastSum);
+        Assert.That(lastSum, Is.EqualTo(0));
+    }
+    
+    // 測試兩種FUNCTION內的質
+    [Test]
+    public void Sum_ByDefault_Returns0_test2()
+    {
+        int lastSum = _memCalculator.Sum();
+        Assert.That(lastSum,Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Add_WhenCalled_ChangesSum()
+    {
+        _memCalculator.Add(1);
+        int sum = _memCalculator.Sum();
+        Assert.That(sum,Is.EqualTo(1));
     }
 }
