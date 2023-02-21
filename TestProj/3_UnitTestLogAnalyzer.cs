28ac8e384c371244eb98b4868f40ea7696a24f8f
ecpay CY <>
2023-02-21 17:58:22 +0800

    未完成

---
+++ TestProj/3_UnitTestLogAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+using UnitTestDemo;
+
+namespace TestProj;
+
+[TestFixture]
+public class UnitTestLogAnalyzer
+{
+    private LogAnalyzer _logAnalyzer_fake;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _logAnalyzer_fake =  new LogAnalyzer(new FakeExtensionManager());
+    }
+
+    [Test]
+    public void IsValidFileName_NameSupportedExtension_ReturnsTrue()
+    {
+        _logAnalyzer_fake
+    }
+    
+    
+
+    
+}
+internal class FakeExtensionManager : IExtensionManager
+{
+    public bool WillBeVaild = false;
+    public bool IsValid(string fileName)
+    {
+        return WillBeVaild;
+    }
+}
---
+++ UnitTestDemo/LogAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿namespace UnitTestDemo;
+
+
+// 此範例為 假設須測試資料為與外界相依 ex:讀取本機內的檔案
+// 若實際用檔案做測試可能會有效能，檔案不存在等狀況，
+// 利用InterFace解決，做出fake data。
+public class LogAnalyzer
+{
+    private IExtensionManager _manager;
+
+    public LogAnalyzer(IExtensionManager manager)
+    {
+        _manager = manager;
+    }
+
+    public bool IsValidLogFileName(string fileName)
+    {
+        return _manager.IsValid(fileName);
+    }
+}
+
+public interface IExtensionManager
+{
+    bool IsValid(string fileName);
+}
